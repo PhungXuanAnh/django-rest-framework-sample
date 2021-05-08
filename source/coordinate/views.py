b6923c554d13ac56a252019b7e8b91189638f19c
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-05-09 00:12:13 +0700

    .

--- source/coordinate/views.py
+++ source/coordinate/views.py
@@ -1,11 +1,9 @@
-from main.paginations.custom_paginations import CustomPagination
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework import filters as rest_filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
-
 
 from coordinate.models import Coordinate
 from coordinate.serializers import CoordinateModelSerializer
@@ -19,6 +17,3 @@ class CoordinateModelViewSet(viewsets.ModelViewSet):
     filter_backends = [rest_filters.OrderingFilter]
     ordering_fields = ['created_at']
     ordering = ['created_at']
-
-    # pagination
-    pagination_class = CustomPagination
