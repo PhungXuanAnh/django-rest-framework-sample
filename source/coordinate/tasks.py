5af9eab0d39c5bb7bfd9f370a69b40ac0f92814d
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-06-05 01:25:25 +0700

    .

--- source/coordinate/tasks.py
+++ source/coordinate/tasks.py
@@ -13,8 +13,8 @@ def check_battery_level():
     latest_coordinate = Coordinate.objects.all().order_by("-created_at").first()
     logger.debug("current battery level: %s", latest_coordinate.battery_level)
     if (
-        latest_coordinate.battery_level <= 10
-        or latest_coordinate.created_at < datetime.datetime.now()
+        latest_coordinate.battery_level <= 20
+        or (datetime.datetime.now().astimezone() - latest_coordinate.created_at).seconds > 300
     ):
         sent_subject = "Battery low or missing battery data"
         sent_text = "battery level: {} at {}".format(
@@ -22,4 +22,5 @@ def check_battery_level():
         )
         send_gmail(sent_subject, sent_text)
         logger.error("Send email at %s", datetime.datetime.now())
+        print('----------')
     return None
--- source/scripts/live-test.py
+++ source/scripts/live-test.py
@@ -7,7 +7,7 @@ import datetime
 import django
 import datetime
 
-sys.path.append(os.path.dirname(__file__) + '/../')
+sys.path.append(os.path.dirname(__file__) + "/../")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings.local")
 django.setup()
 
@@ -22,27 +22,32 @@ from music.tasks import sample_music_task
 
 
 def test_coordinate_app():
-    for coo in Coordinate.objects.all().order_by('created_at'):
+    for coo in Coordinate.objects.all().order_by("created_at"):
         print(coo.created_at)
         print(coo.battery_level)
 
-    latest_coordinate = Coordinate.objects.all().order_by('-created_at').first()
+    latest_coordinate = Coordinate.objects.all().order_by("-created_at").first()
     print(latest_coordinate.created_at)
     print(latest_coordinate.battery_level)
+
 
 def test_music_app():
     musican = Musician.objects.get(id=1)
     print(musican.album_set.all())
+
 
 def test_celery_sample_task():
     # sample_task.apply_async(queue="queue-low")
     # sample_music_task.apply_async(queue="queue-high")
     # sample_task.apply_async()
     # sample_music_task.apply_async()
-    check_battery_level.apply_async(queue="queue-high")
-    # check_battery_level()
+    # check_battery_level.apply_async(queue="queue-high")
+    check_battery_level()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_celery_sample_task()
     # test_coordinate_app()
+
+    latest_coordinate = Coordinate.objects.all().order_by("-created_at").first()
+    print((datetime.datetime.now().astimezone() - latest_coordinate.created_at).seconds)
