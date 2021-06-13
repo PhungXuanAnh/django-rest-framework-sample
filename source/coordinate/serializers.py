31a9ea8901bd60bbde48b9c7da0ef969ba2f40ae
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-06-13 12:02:20 +0700

    return created at at Vietname timezone

--- source/coordinate/serializers.py
+++ source/coordinate/serializers.py
@@ -1,8 +1,16 @@
+import pytz
 from rest_framework import serializers
 from coordinate.models import Coordinate
 
 
 class CoordinateModelSerializer(serializers.ModelSerializer):
+    created_at = serializers.SerializerMethodField()
+
     class Meta:
         model = Coordinate
-        fields = ['latitude', 'longitude', 'created_at', 'battery_level']
+        fields = ["latitude", "longitude", "created_at", "battery_level"]
+
+    def get_created_at(self, obj):
+        return obj.created_at.astimezone(pytz.timezone("Asia/Ho_Chi_Minh")).strftime(
+            "[%Y-%m-%d]-[%H:%M:%S]"
+        )
