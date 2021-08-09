256cc1a664c873ec876961dea7fddca920c7a490
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-08-09 17:15:53 +0700

    Exception: sample customized exception and return message base on accepted language from request

--- source/main/custom_permissions.py
+++ source/main/custom_permissions.py
@@ -1,7 +1,22 @@
 import debugpy
 from rest_framework import permissions
 
+from main.customized_exceptions import Custom403Exception
+
+
 class CustomPermission(permissions.IsAuthenticated):
+
     def has_permission(self, request, view):
+        user = request.user
+        if not user:
+            # return msg base on language accept from request
+            language = request.headers.get('Accept-Language')
+            if language == "EN":
+                detail = "This feature is for advocate only"
+            else:
+                detail = "Chức năng này chỉ dành cho cán bộ bán"
+
+            raise Custom403Exception(detail=detail)
+
         debugpy.breakpoint()
         return super().has_permission(request, view)
