1295c378f0ee7e14a5b22859500037c0f43c5a4f
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-02-25 11:59:40 +0700

    fix static file not found

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from swagger.views import schema_view as swagger_view
 
 
@@ -32,3 +34,11 @@ urlpatterns = [
 ]
 
 urlpatterns += api_v1_urls
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
