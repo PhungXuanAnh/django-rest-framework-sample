dd7560290b6cb1aa74e9381e525f39ab1febc169
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-02-26 10:44:12 +0700

    sentry: add celery integration

--- source/main/settings/base.py
+++ source/main/settings/base.py
@@ -234,6 +234,7 @@ if DEBUG:
 # pylint: disable=wrong-import-position
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
 
 SENTRY_DSN = env(
     "SENTRY_DSN", default=""
@@ -241,11 +242,12 @@ SENTRY_DSN = env(
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
-    integrations=[DjangoIntegration()],
+    integrations=[DjangoIntegration(), CeleryIntegration()],
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+    # traces_sample_rate=1.0,     # 100%
+    traces_sample_rate=0.2,     # 20%
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
