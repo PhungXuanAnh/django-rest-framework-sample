b765832a310e2be17fdab4cf464a709202741126
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-03-02 15:31:13 +0700

    swagger: add support https

--- source/swagger/views.py
+++ source/swagger/views.py
@@ -2,6 +2,23 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    """
+    This class for using both HTTP and Https in swagger
+    reference: https://stackoverflow.com/a/68021739/7639845
+
+    Another way is using url as comment bellow
+    """
+
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["https", "http"]  # https is default
+        # schema.schemes = ["http", "https"]  # http is default
+        return schema
+
 
 SwaggerView = get_schema_view(
     openapi.Info(
@@ -11,8 +28,9 @@ SwaggerView = get_schema_view(
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
-        # url='https://example.net/api/v1/', # Base URL, reference: https://stackoverflow.com/a/56090744/7639845
     ),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+    # url='https://example.net/api/v1/', # Base URL, reference: https://stackoverflow.com/a/56090744/7639845
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
