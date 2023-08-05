506af732925d2f4cfdda344fcd84d0bb2d59a755
Phung Xuan Anh <phungxuananh1991@gmail.com>
2023-08-05 15:20:55 +0700

    add fucntion to show database query sql of queryseet

--- source/temp.py
+++ source/temp.py
@@ -19,6 +19,18 @@ from music.models import Album, Musician, Profile
 from music.tasks import sample_music_task
 
 
+def test_hightlight_of_todo_tree_extension():
+    # TODO: 
+    
+    # NOTE:
+    
+    # [x]
+    
+    # [ ] 
+    # FIXME:
+    pass
+
+
 def test_music_app():
     musican = Musician.objects.get(id=1)
     print(musican.album_set.all())
@@ -40,7 +52,66 @@ def test_crontab_parser():
     print("months_of_year: ", crontab_parser(12, 1).parse("*/2"))
     print("months_of_year: ", crontab_parser(12, 1).parse("2-12/2"))
 
+    
+def steps_to_test_django_model_in_django_shell():
+    """ purpose of this work is to see how django convert ORM to SQL"""
+    # pip install ipython
+    # pip install sqlparse
+    # open django shell: python manage.py  shell
+    # declare model
+    import sqlparse
+    from django.db import models
+    class Customer(models.Model):
+        id = models.IntegerField(db_column='ID', primary_key=True)  # Field name made lowercase.
+        age = models.IntegerField(...)
+        name = models.CharField(...)
+
+        class Meta:
+            db_table = 'Customer'
+            managed = False # NOTE: add this config to ignore create real table, reference: https://stackoverflow.com/a/60346675/7639845
+            app_label  = "django.contrib.auth", # NOTE: we need to register this model with at least one INSTALLED_APPS, reference: https://stackoverflow.com/a/62772698/7639845 
+            
+    # now do write your django queryset and print SQL
+    qs = Customer.objects.filter(age__gte=25)
+    print(qs.query) # reference: https://stackoverflow.com/a/1074224/7639845
+    print("---------------------------------")
+    print(sqlparse.format(str(qs.query), reindent=True, keyword_case='upper'))
+   
+ 
+def test_select_related_and_prefetch_related_real_sql():
+    from django.db import models
+    import sqlparse
+    
+    class ModelA(models.Model):
+        class Meta:
+            db_table = 'ModelA'
+            managed = False # NOTE: add this config to ignore create real table, reference: https://stackoverflow.com/a/60346675/7639845
+            app_label  = "django.contrib.auth", # NOTE: we need to register this model with at least one INSTALLED_APPS, reference: https://stackoverflow.com/a/62772698/7639845 
+        
+
+    class ModelB(models.Model):
+        a = models.ForeignKey(ModelA, on_delete=models.CASCADE)
+        class Meta:
+            db_table = 'ModelB'
+            managed = False # NOTE: add this config to ignore create real table, reference: https://stackoverflow.com/a/60346675/7639845
+            app_label  = "django.contrib.auth", # NOTE: we need to register this model with at least one INSTALLED_APPS, reference: https://stackoverflow.com/a/62772698/7639845 
+        
+
+    print("Forward ForeignKey relationship")
+    b = ModelB.objects.select_related('a').all()
+    print(sqlparse.format(str(b.query), reindent=True, keyword_case='upper'))
+    print("----------------------------------------------------------------")
+
+    print("Reverse ForeignKey relationship")
+    a = ModelA.objects.prefetch_related('modelb_set').all()
+    print(sqlparse.format(str(a.query), reindent=True, keyword_case='upper'))
+    
+    from django.db import connection
+    print(connection.queries)
+    # b.objects.select_related('a').first()
 
 if __name__ == "__main__":
     # test_celery_sample_task()
-    test_crontab_parser()
+    # test_crontab_parser()
+    # steps_to_test_django_model_in_django_shell()
+    test_select_related_and_prefetch_related_real_sql()
